04a4df0b2ca59493419bfef5b562adc74e407b3d
KyraHeisler <Kyra.Heisler@Student.hcca.ca>
2025-11-28 10:41:35 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,36 +1,47 @@
-const WebSocket = require("ws");
-const server = new WebSocket.Server({ port: 8080 });
+// Install first: npm install express socket.io
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+
+app.use(express.static("public"));
 
 let players = {};
 
-server.on("connection", ws => {
-    const id = Date.now().toString();
-    players[id] = { x: 0, y: 1, z: 0 };
+io.on("connection", (socket) => {
+    console.log("A player connected:", socket.id);
 
-    ws.send(JSON.stringify({ type: "init", id, players }));
+    // Add player
+    players[socket.id] = {
+        x: 400,
+        y: 300,
+        name: "Player-" + socket.id.substring(0, 4),
+        color: "#" + Math.floor(Math.random() * 16777215).toString(16)
+    };
 
-    ws.on("message", msg => {
-        const data = JSON.parse(msg);
+    // Send all players to new connection
+    socket.emit("currentPlayers", players);
 
-        if (data.type === "move") {
-            players[id] = data.position;
+    // Notify others of new player
+    socket.broadcast.emit("newPlayer", players[socket.id]);
 
-            broadcast(JSON.stringify({
-                type: "playerMove",
-                id,
-                position: data.position
-            }));
+    // Movement handler
+    socket.on("move", (data) => {
+        if (players[socket.id]) {
+            players[socket.id].x = data.x;
+            players[socket.id].y = data.y;
+            io.emit("playerMoved", players[socket.id]);
         }
     });
 
-    ws.on("close", () => {
-        delete players[id];
-        broadcast(JSON.stringify({ type: "playerLeft", id }));
+    // Remove player when disconnected
+    socket.on("disconnect", () => {
+        console.log("Player disconnected:", socket.id);
+        delete players[socket.id];
+        io.emit("playerDisconnected", socket.id);
     });
 });
 
-function broadcast(msg) {
-    server.clients.forEach(client => client.send(msg));
-}
-
-console.log("Server running at ws://localhost:8080");
+http.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
+});
